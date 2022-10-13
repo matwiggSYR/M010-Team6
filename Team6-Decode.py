34ae6d9b62ddc9443706d640292645782225f44d
matwiggSYR <115006854+matwiggSYR@users.noreply.github.com>
2022-10-13 19:52:48 -0400

    Update Team6-Decode.py

--- Team6-Decode.py
+++ Team6-Decode.py
@@ -1,44 +1,37 @@
 import makeDictionary
-def Code(txtFile):
-    # opens and stores what is in the txtfile in the string "myStr"
-    myTxtFile = open(txtFile) 
-    myStr = myTxtFile.read()
-   
-    # storing the key for the dictionary Char2Bin, because Code converts characters to binary
-    myKey = makeDictionary.getCodeMap('Char2Bin')
-    
-    # for loop that iterates over each char in the string and uses the dictionary to get the coresponding binary number. Binary number is then appended to the list 
-    binaryList = []
-    for i in myStr:
-        binaryList.append(myKey.get(i))
-    
-    # converts the list into a string
-    binaryStr = ''.join(binaryList)
-    
-    # for loop that calculates the bit value. iterates over each char in the string and adds it to the total sum
-    myDec = 0
-    for i in myStr:
-        temp = str(myKey.get(i))
-        myDec = str(myDec)
-        myDec = bin(int(myDec,2) + int(temp,2))[2:]
-    
-    # converts the sum to integer and rounds it to base
-    myDec = int(myDec,2)
-    myDec = round(myDec/10)
-    
-    # converts decimal bit to string
-    myDecStr = str(myDec)
-    
-    # creates the output format of D.B
-    myOutput = myDecStr+"."+binaryStr
-    
-    # creates output file and writes the output in the file
-    out_file = open('bin_output.txt','w+')
-    out_file.write(myOutput)
+def Decode(txtFile):
+    # opens and stores what is in the bin_output.txt file in the string "inputBin"
+    myTxtFile = open(txtFile)
+    inputBin = myTxtFile.read()
+
+    # storing the key for the dictionary Char2Bin, because Code converts binary to characters
+    myKey = makeDictionary.getCodeMap('bin2char')
+
+    # converts the input string into a list and then splits the decimal value from the binary value
+    binList = inputBin.split('.')
+
+    # stores the decimal value in inputBin
+    inputBin = binList[1]
+
+    # for loop that iterates over binary numbers and uses the dictionary to convert it to the corresponding character
+    # uses 0 and 1 as flags to see if the binary number is a short or long
+    # stores the characters in myDecode
+    myDecode = ""
+    lastIndex = 0
+    for i in range(len(inputBin)):
+        i = i + lastIndex
+        if i >= len(inputBin):
+            break
+        if inputBin[i].startswith("1"):
+            myDecode = myDecode + str(myKey.get(inputBin[i: (i + 7)]))
+            lastIndex = 6 + lastIndex
+        elif inputBin[i].startswith("0"):
+            myDecode = myDecode + str(myKey.get(inputBin[i: (i + 5)]))
+            lastIndex = 4 + lastIndex
+
+    # creates output file and writes the decoded word in the file
+    out_file = open('TextOutput.txt', 'w+')
+    out_file.write(myDecode)
 
 
-# enter your file name below
-Code("*INPUT_FILENAME_.txt")
-
-
-
+Decode("bin_output.txt")
