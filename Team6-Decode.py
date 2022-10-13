54fcbff065f163246f9c21e4639ac53e1f013ab1
matwiggSYR <115006854+matwiggSYR@users.noreply.github.com>
2022-10-13 19:52:33 -0400

    Update Team6-Decode.py

--- Team6-Decode.py
+++ Team6-Decode.py
@@ -1,26 +1,44 @@
 import makeDictionary
-def Decode(txtFile):
-    myTxtFile = open(txtFile)
-    inputBin = myTxtFile.read()
-    myKey = makeDictionary.getCodeMap('bin2char')
-    binList = inputBin.split('.')
-    inputBin = binList[1]
-    print("bin:",inputBin)
-    myDecode = ""
-    lastIndex = 0
-    for i in range(len(inputBin)):
-        i = i + lastIndex
-        if i >= len(inputBin):
-            break
-        if inputBin[i].startswith("1"):
-            myDecode = myDecode + str(myKey.get(inputBin[i: (i + 7)]))
-            lastIndex = 6 + lastIndex
-        elif inputBin[i].startswith("0"):
-            myDecode = myDecode + str(myKey.get(inputBin[i: (i + 5)]))
-            lastIndex = 4 + lastIndex
-    print("myDecode:",myDecode)
-    out_file = open('TextOutput.txt', 'w+')
-    out_file.write(myDecode)
+def Code(txtFile):
+    # opens and stores what is in the txtfile in the string "myStr"
+    myTxtFile = open(txtFile) 
+    myStr = myTxtFile.read()
+   
+    # storing the key for the dictionary Char2Bin, because Code converts characters to binary
+    myKey = makeDictionary.getCodeMap('Char2Bin')
+    
+    # for loop that iterates over each char in the string and uses the dictionary to get the coresponding binary number. Binary number is then appended to the list 
+    binaryList = []
+    for i in myStr:
+        binaryList.append(myKey.get(i))
+    
+    # converts the list into a string
+    binaryStr = ''.join(binaryList)
+    
+    # for loop that calculates the bit value. iterates over each char in the string and adds it to the total sum
+    myDec = 0
+    for i in myStr:
+        temp = str(myKey.get(i))
+        myDec = str(myDec)
+        myDec = bin(int(myDec,2) + int(temp,2))[2:]
+    
+    # converts the sum to integer and rounds it to base
+    myDec = int(myDec,2)
+    myDec = round(myDec/10)
+    
+    # converts decimal bit to string
+    myDecStr = str(myDec)
+    
+    # creates the output format of D.B
+    myOutput = myDecStr+"."+binaryStr
+    
+    # creates output file and writes the output in the file
+    out_file = open('bin_output.txt','w+')
+    out_file.write(myOutput)
 
 
-Decode("bin_output.txt")
+# enter your file name below
+Code("*INPUT_FILENAME_.txt")
+
+
+
